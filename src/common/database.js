2627f4f973bfab7b2ca2fb1626735423917a3b05
thang5555 <148266104+thang5555@users.noreply.github.com>
2024-03-23 18:07:09 +0700

    Update database.js

--- src/common/database.js
+++ src/common/database.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose");
 module.exports = ()=>{
     mongoose
-    .connect("mongodb://127.0.0.1:27017/deevisco ")
+    .connect("mongodb+srv://thang5555:ANHanh9x@deeviscomongodb.ygmbzai.mongodb.net/deevisco")
     .then(() => console.log("Connected"));
     return mongoose;
 };
